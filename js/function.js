788c53de8b3435731a5e38c7a546dc851d5c05a5
finest-cy <finest-cy@outlook.com>
2021-10-03 04:31:28 +0800

    Update function.js

--- js/function.js
+++ js/function.js
@@ -324,6 +324,10 @@ function searchBox() {
     '    </div>' + 
     '    <div class="radio-group" id="music-source">' + 
     '       <label><input type="radio" name="source" value="netease" checked=""> 网易云</label>' + 
+    '       <label><input type="radio" name="source" value="tencent"> QQ</label>' + 
+    '       <label><input type="radio" name="source" value="xiami"> 虾米</label>' + 
+    '       <label><input type="radio" name="source" value="kugou"> 酷狗</label>' + 
+    '       <label><input type="radio" name="source" value="baidu"> 百度</label>' +
     '   </div>' + 
     '</div></form>';
     layer.open({
